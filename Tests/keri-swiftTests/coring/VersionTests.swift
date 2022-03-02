73f3783dbe8cc41892ffb9f31f7045570d730291
Kevin Griffin <griffin.kev@gmail.com>
2022-03-01 19:59:14 -0500

    fixes versify

--- Tests/keri-swiftTests/coring/VersionTests.swift
+++ Tests/keri-swiftTests/coring/VersionTests.swift
@@ -10,19 +10,19 @@ final class VersionTests: XCTestCase {
     func testVersify() throws {
         var vsy = ""
 
-        XCTAssertNoThrow(vsy = versify(ident: .keri, version: Version, kind: .json, size: 0))
-        XCTAssertEqual(vsy, "KERI10JSON000000_")
+        XCTAssertNoThrow(vsy = versify(ident: .keri, version: Version, kind: .json, size: 284))
+        XCTAssertEqual(vsy, "KERI10JSON00011c_")
     }
 
     func testDeversify() throws {
-        let v = try XCTUnwrap(try deversify(vs: "KERI10JSON000000_"))
+        let v = try XCTUnwrap(try deversify(vs: "KERI10JSON00011c_"))
         XCTAssertNotNil(v)
 
         XCTAssertEqual(v.ident, .keri)
         XCTAssertEqual(v.kind, .json)
         XCTAssertEqual(v.version.major, 1)
         XCTAssertEqual(v.version.minor, 0)
-        XCTAssertEqual(v.size, 0)
+        XCTAssertEqual(v.size, 284)
     }
 
     func testDeversifyEmpty() throws {
